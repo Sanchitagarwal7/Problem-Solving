416d99d8ea71748708c446ee389d81d56af29da7
Sanchit Agarwal <sanchita.g893@gmail.com>
2024-03-12 22:59:22 +0530

    searching-2 hw (updated binary search)

--- Searching-2/Get-Equal-Substrings-Within-Budget.cpp
+++ Searching-2/Get-Equal-Substrings-Within-Budget.cpp
@@ -1,29 +1,70 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int equalSubstring(string s, string t, int maxCost) {
-    int n = s.length();
+// int equalSubstring(string s, string t, int maxCost) {
+//     int n = s.length();
 
-    //Couldn't come up with binary search approach as sliding window was more intuitive for me
+//     //Couldn't come up with binary search approach as sliding window was more intuitive for me
 
-    int low = 0;
+//     int low = 0;
 
-    int maxi = -1e9;
+//     int maxi = -1e9;
 
-    int cost = 0;
+//     int cost = 0;
 
-    for(int high=0; high<n; high++){
-        cost += abs(s[high]-t[high]);
+//     for(int high=0; high<n; high++){
+//         cost += abs(s[high]-t[high]);
 
-        while(low<=high && cost > maxCost){
-            cost -= abs(s[low]-t[low]);
-            low++;
+//         while(low<=high && cost > maxCost){
+//             cost -= abs(s[low]-t[low]);
+//             low++;
+//         }
+
+//         maxi = max(maxi, high-low+1);
+//     }
+//     return maxi;
+// }
+
+bool isPossible(string &s, string &t, int &maxCost, int size, int &n){
+        int low = 0;
+        int cost = 0;
+        int len = 0;
+
+        for(int high = 0; high<n; high++){
+            cost += abs(s[high]-t[high]);
+
+            while(low<=high && cost>maxCost){
+                cost -= abs(s[low]-t[low]);
+                low++;
+            }
+
+            if(cost<=maxCost){
+                len = max(len, high-low+1);
+            }
+        }
+        return len >= size;
+    }
+    int equalSubstring(string s, string t, int maxCost) {
+        //edit: finally did it with BinarySearch
+        int n = s.length();
+
+        int low = 1;
+        int high = n;
+
+        int ans = 0;
+        while(low<=high){
+            int mid = low+(high - low)/2;
+
+            if(isPossible(s, t, maxCost, mid, n)){
+                ans = mid;
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
         }
 
-        maxi = max(maxi, high-low+1);
+        return ans;
     }
-    return maxi;
-}
 
 int main(){
     string s = "abcd";

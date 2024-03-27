60994c54388b91b099d4d852ddd65b0491fa4c6a
Sanchit Agarwal <sanchita.g893@gmail.com>
2024-03-28 00:15:27 +0530

    LCA of a Binary Tree hw(remaining)

---
+++ Trees-3/Lowest-Common-Ancestor-of-a-Binary-Tree.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root == NULL)return root;
+    if(root == p || root == q){
+        return root;
+    }
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if(left!=NULL && right!=NULL){
+        return root;
+    }
+    if(left!=NULL && right==NULL){
+        return left;
+    }
+    if(left==NULL && right!=NULL){
+        return right;
+    }
+
+    return NULL;
+}
+
+int main(){
+    return 0;
+}
